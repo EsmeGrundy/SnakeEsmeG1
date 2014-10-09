f2d471f37fb22c83b60dfd91f165ed8b8668aab5
Esme Grundy <eg2485@gmail.com>
2014-10-09 10:44:22 -0700

    Updated Portfolio 2/Starting to create arrow keys

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,14 +1,31 @@
+/*--------------------------------------------------------------------------
+ * Variables
+ * -------------------------------------------------------------------------
+ */
+
 var snake;
 var snakeLength;
 var snakeSize;
+
+var food;
 
 var context;
 var ScreenHeight;
 var ScreenWidth;
 
+/*--------------------------------------------------------------------------
+ * Calling Functions
+ * -------------------------------------------------------------------------
+ */
 gameInitialize();
 snakeInitialize();
-setInterval(gameLoop, 1000/30);
+foodInitialize();
+setInterval(gameLoop, 1000/20);
+
+/*-------------------------------------------------------------------------
+ * Game Functions
+ * ------------------------------------------------------------------------
+ */
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -19,18 +36,26 @@ function gameInitialize() {
 
     canvas.width = ScreenWidth;
     canvas.height = ScreenHeight;
+    
+    var direction = "right";
 }
 
 function gameLoop() {
     gameDraw();
     snakeUpdate();
     snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
     context.fillStyle = "rgb(235, 150, 54)";
     context.fillRect(0, 0, ScreenWidth, ScreenHeight);
 }
+
+/*-------------------------------------------------------------------------
+ * Snake Functions
+ * ------------------------------------------------------------------------
+ */
 
 function snakeInitialize() {
     snake = [];
@@ -49,6 +74,8 @@ function snakeDraw() {
     for (var index = 0; index < snake.length; index++) {
         context.fillStyle = "rgb(0, 0, 0)";
         context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+        context.strokeStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
     }
 }
 
@@ -61,7 +88,39 @@ function snakeUpdate() {
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+    
+    if(direction === "right")snakeHeadX++;
+    else if(direction === "left")snakeHeadX--;
+     else if(direction === "up")snakeHeadY++;
+      else if(direction === "down")snakeHeadY--;
+    
 
 }
 
+/*--------------------------------------------------------------------------
+ * Food Functions
+ * -------------------------------------------------------------------------
+ */
+
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw() {
+    context.fillStyle = "black";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * ScreenWidth);
+    var randomY = Math.floor(Math.random() * ScreenHeight);
+    
+    food.x = randomX;
+    food.y = randomY;
+}
 
